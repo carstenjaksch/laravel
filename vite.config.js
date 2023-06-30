0fbd308bca8f12765c51b3fcd71f637b00927fa4
Carsten Jaksch <carsten@jaksch.me>
2023-06-30 13:01:13 +0200

    Add DDEV and fix Vite

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,10 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    server: {
+        hmr: {
+            host: process.env.DDEV_HOSTNAME,
+            protocol : 'wss',
+        }
+     },
 });
